fdecfb73f53b43f1f122649dc08b022709eb1ab3
mois laptop <sashankbanda>
2025-08-28 05:08:09 +0530

    http://localhost:8000 to https://budgetplannerbackend.onrender.com

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'http://localhost:8000';
+const BACKEND_URL = 'https://budgetplannerbackend.onrender.com';
 const API = `${BACKEND_URL}/api`;
 
 const apiClient = axios.create({
